53447d7f2f218eb4f1e63a35eca6c4b9d906d814
Unknown <thiago.empreendedor01@gmail.com>
2018-05-28 19:25:15 -0300

    CidadeRepository implementando JPARepository
    Foram feitas as alterações pedidas na demanda, e testados as funcionalidades após a alteração.

--- src/main/java/br/com/cajumobile/delivery/repository/CidadeRepository.java
+++ src/main/java/br/com/cajumobile/delivery/repository/CidadeRepository.java
@@ -1,30 +1,10 @@
 package br.com.cajumobile.delivery.repository;
 
 import br.com.cajumobile.delivery.model.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public class CidadeRepository extends GenericRepository<Cidade> {
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
 
-    public CidadeRepository() {
-        super(Cidade.class);
-    }
-
-    public List<Cidade> listCidades() {
-        return entityManager.createQuery("FROM Cidade c", Cidade.class).getResultList();
-    }
-
-    public void deleteById(Integer idCidade) {
-        entityManager.createQuery("DELETE FROM Cidade c WHERE c.id = :idCidade")
-                .setParameter("idCidade", idCidade)
-                .executeUpdate();
-    }
-
-    public Cidade findById(Integer idCidade) {
-        return entityManager.createQuery("FROM Cidade c WHERE c.id = :id", Cidade.class)
-                .setParameter("id", idCidade)
-                .getSingleResult();
-    }
 }
